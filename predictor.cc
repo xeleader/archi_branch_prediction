f0805d6382c4cc55dfa386057771794af3e6e14c
Alexandre Blervaque <Alexandre.Blervaque@phelma.grenoble-inp.fr>
2017-12-21 18:44:04 +0100

    derniers changements

--- predictor.cc
+++ predictor.cc
@@ -44,12 +44,28 @@ PREDICTOR::PREDICTOR(char *prog, int argc, char *argv[])
 
 
       nentries = (1 << pcbits);        // nombre d'entrées dans la table
-      pcmask   = (nentries - 1);       // masque pour n'accéder qu'aux bits significatifs de PC
       countmax = (1 << countbits) - 1; // valeur max atteinte par le compteur à saturation
       table    = new uint32_t[nentries]();
+		historic = 0;
       mask_hist = (nentries - 1);
 
    #endif
+
+	 ///////////////////////////////////////////
+
+	#if gshare
+
+	   uint32_t pcbits    = strtoul(argv[0], NULL, 0);
+	   uint32_t countbits = strtoul(argv[1], NULL, 0);
+
+
+	   nentries = (1 << pcbits);        // nombre d'entrées dans la table
+	   countmax = (1 << countbits) - 1; // valeur max atteinte par le compteur à saturation
+	   table    = new uint32_t[nentries]();
+		historic = 0;
+	   mask = (nentries - 1);
+
+	#endif
 
 }
 
@@ -83,7 +99,7 @@ void PREDICTOR::UpdatePredictor(UINT64 PC, OpType opType, bool resolveDir, bool 
 
 bool PREDICTOR::GetPrediction(UINT64 PC)
 {
-   uint32_t v = table[historic & pcmask];
+   uint32_t v = table[historic & mask_hist];
    return (v > (countmax / 2)) ? TAKEN : NOT_TAKEN;
 }
 
@@ -98,7 +114,34 @@ void PREDICTOR::UpdatePredictor(UINT64 PC, OpType opType, bool resolveDir, bool 
    else if( (v == (countmax-1)/2) && (resolveDir == TAKEN) ) table[historic & mask_hist] = countmax;
    else table[historic & mask_hist] = (resolveDir == TAKEN) ? SatIncrement(v, countmax) : SatDecrement(v);
 
-   cout << "Historique : " << historic << endl;
+   // cout << "Historique : " << historic & mask_hist << endl;
+}
+
+#endif
+
+/////////////////////////////////////////////////////////////
+/////////////////////// GSHARE //////////////////////////////
+
+#if gshare
+
+bool PREDICTOR::GetPrediction(UINT64 PC)
+{
+   uint32_t v = table[(PC ^ historic) & mask];
+   return (v > (countmax / 2)) ? TAKEN : NOT_TAKEN;
+}
+
+/////////////////////////////////////////////////////////////
+
+void PREDICTOR::UpdatePredictor(UINT64 PC, OpType opType, bool resolveDir, bool predDir, UINT64 branchTarget)
+{
+   uint32_t v = table[(PC ^ historic) & mask];
+   historic = (mask) & ((resolveDir == TAKEN) ? (historic << 1)+1 : (historic << 1));
+
+   if( (v == (countmax+1)/2) && (resolveDir == NOT_TAKEN) ) table[(PC ^ historic) & mask] = 0;
+   else if( (v == (countmax-1)/2) && (resolveDir == TAKEN) ) table[(PC ^ historic) & mask] = countmax;
+   else table[(PC ^ historic) & mask] = (resolveDir == TAKEN) ? SatIncrement(v, countmax) : SatDecrement(v);
+
+   // cout << "Historique : " << historic & mask_hist << endl;
 }
 
 #endif
